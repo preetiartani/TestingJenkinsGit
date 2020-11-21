4fa8e786dc029f19f78b4551d4accf4988430985
preetiartani <42246501+preetiartani@users.noreply.github.com>
2020-11-21 11:57:03 +0530

    Add files via upload

---
+++ src/com/lti/entity/Student.java
@@ -0,0 +1,31 @@
+package com.lti.entity;
+
+public class Student {
+	int stuId;
+	String stuName;
+	double stuFees;
+	public int getStuId() {
+		return stuId;
+	}
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	public double getStuFees() {
+		return stuFees;
+	}
+	public void setStuFees(double stuFees) {
+		this.stuFees = stuFees;
+	}
+	@Override
+	public String toString() {
+		return "Student [stuId=" + stuId + ", stuName=" + stuName + ", stuFees=" + stuFees + "]";
+	}
+	
+	
+}
---
+++ src/com/lti/user/StudentApp.java
@@ -0,0 +1,16 @@
+package com.lti.user;
+
+import com.lti.entity.Student;
+
+public class StudentApp {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setStuId(101);
+		s1.setStuName("Vijay");
+		s1.setStuFees(8000);
+		System.out.println(s1.getStuId());
+		System.out.println(s1.getStuName());
+		System.out.println(s1.getStuFees());
+		System.out.println(s1);
+	}
+}
